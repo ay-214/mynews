1f73a504e1d26539d4ea9fe9a35380660be2f549
ito ayumi <t.s2.ovo.uvu@gmail.com>
2024-08-30 11:09:31 +0000

    PHP/Laravel 10 の課題４を作成

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+/*PHP/Laravel 10 課題４：課題３の答えを実際に作成してみましょう。また、作成したBladeファイルにhtmlで記述して装飾してみましょう*/
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>profile</title>
+
+        <style>
+           h1 {
+           color: red;
+           padding: 100px 200px;
+           }
+        </style>
+
+    </head>
+    <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+            <h1>profileのcreate作成画面</h1>
+            </div>
+        </div>
+    </div>
+    </body>
+</html>
