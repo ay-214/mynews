ad1d11bd87968fff34369ab65df426782e8d5772
ito ayumi <t.s2.ovo.uvu@gmail.com>
2024-08-30 11:16:10 +0000

    PHP/Laravel 10 の課題４【edit Actionを描画するファイル】を作成

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+/*PHP/Laravel 10 課題4:課題3の答えを実際に作成してみましょう。また、作成したBladeファイルにhtmlで記述して装飾してみましょう*/
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>edit</title>
+
+        <style>
+            h1 {
+                color: #800080;
+                font-weight: bold;
+            }
+        </style>
+        
+    </head>
+    <body>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8 mx-auto">
+            <h1>編集ページ作成画面</h1>
+            </div>
+          </div>
+        </div>
+    </body>
+</html>
