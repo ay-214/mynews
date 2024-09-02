cc5d4c0846e6bba7e7c157d9d676f212c3fe6ed1
ito ayumi <t.s2.ovo.uvu@gmail.com>
2024-09-02 10:28:32 +0000

    PHP/Laravel 12 の課題１を作成

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -48,7 +48,29 @@
                         </ul>
 
                         <!-- Right Side Of Navbar -->
+                        {{--PHP/Laravel 12 課題1  resources/views/layouts/profile.blade.php も編集して、 プロフィール編集画面にもログインリンクやログアウトリンクを表示するようにカスタマイズしましょう↓  --}}
                         <ul class="navbar-nav">
+                            @guest
+                            <li><a class="nav-link" href="{{ route('login') }}">{{ __('messages.login') }}</a></li>
+                            @else
+                            <li class="nav-item dropdown">
+                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                 </a>
+
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('messages.logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                            @endguest
                         </ul>
                     </div>
                 </div>
