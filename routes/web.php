c6906f84bae3781acd9c044fc0d6399cb89748ee
ito ayumi <t.s2.ovo.uvu@gmail.com>
2024-09-02 10:31:12 +0000

    PHP/Laravel 12 課題２、課題３ を作成

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add');
+    Route::get('news/create', 'add')->middleware('auth');
 });
 
 
@@ -38,8 +38,9 @@ echo ("\n");
 use App\Http\Controllers\Admin\ProfileController;
 Route::controller(ProfileController::class)
 ->prefix('admin')->group(function() {
-    Route::get('profile/create','add');
-    Route::get('profile/edit','edit');
+    /*PHP/Laravel 12 課題２、課題３↓*/
+    Route::get('profile/create','add')->middleware('auth');
+    Route::get('profile/edit','edit')->middleware('auth');
 });
 Auth::routes();
 
