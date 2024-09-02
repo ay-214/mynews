235a35240371d3241082c4d70bd54b789f88dacd
ito ayumi <t.s2.ovo.uvu@gmail.com>
2024-09-02 03:32:03 +0000

    PHP/Laravel 11 課題4を作成

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,29 +1,21 @@
 <!DOCTYPE html>
-/*PHP/Laravel 10 課題4:課題3の答えを実際に作成してみましょう。また、作成したBladeファイルにhtmlで記述して装飾してみましょう*/
+/*PHP/Laravel 11 課題4:【応用】 プロフィール作成画面用に、resources/views/admin/profile/create.blade.php ファイルを作成し、3. で作成した profile.blade.phpファイルを読み込み、また プロフィールのページであることがわかるように titleとcontentを編集しましょう（ヒント: resources/views/admin/news/create.blade.php を参考にします）*/
 
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>profile</title>
+@extends('layouts.profile')
 
-        <style>
-           h1 {
-           color: red;
-           padding: 100px 200px;
-           }
-        </style>
+@section('title', 'プロフィールタイトル')
 
-    </head>
-    <body>
+@section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-            <h1>profileのcreate作成画面</h1>
+                <h2>プロフィールタイトル</h2>
             </div>
         </div>
     </div>
-    </body>
-</html>
+@endsection
+
+
+
+
