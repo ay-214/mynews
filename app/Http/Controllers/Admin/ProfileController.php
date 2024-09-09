2753904cffeedf96ff338aa21fe1c2a8223fa26f
ito ayumi <t.s2.ovo.uvu@gmail.com>
2024-09-09 05:08:07 +0000

    PHP/Laravel14 課題７の修正

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -26,13 +26,6 @@ class ProfileController extends Controller
         $profile = new Profile;
         $form = $request->all();
 
-        if (isset($form['image'])) {
-            $path = $request->file('image')->store('public/image');
-            $profile->image_path = basename($path);
-        } else {
-            $profile->image_path = null;
-        }
-
         unset($form['_token']);
         unset($form['image']);
 
