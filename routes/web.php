6e4fb60c11b0ec96b980923b8bc6aa5cfdbacad4
ito ayumi <t.s2.ovo.uvu@gmail.com>
2024-09-16 07:43:12 +0000

    //PHP/Laravel 19 課 題で作成

--- routes/web.php
+++ routes/web.php
@@ -16,27 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
     Route::get('news/create', 'add')->name('news.add');
     Route::post('news/create','create')->name('news.create');
 });
 
-
-/*PHP/Laravel 09 Routingについて理解する課題３　「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください */
-
-/*use App\Http\Controllers\Admin\AAAControroller;
-Route::controller(AAAController::class)
-->prefix('admin')->group(function() {
-    Route::get('/XXX','bbb');
-});
-*/
-echo ("\n");
-
 /*課題４ 【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。web.phpを編集して、admin/profile/create にアクセスしたら ProfileController の add Action に、admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください
  */
 
- /*PHP/Laravel 13 課題３・６↓*/
+ 
 use App\Http\Controllers\Admin\ProfileController;
 Route::controller(ProfileController::class)
 ->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
@@ -47,5 +37,68 @@ Route::controller(ProfileController::class)
 });
 Auth::routes();
 
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('news/create', 'add')->name('news.add');
+    Route::post('news/create', 'create')->name('news.create');
+    Route::get('news', 'index')->name('news.index');
+});
+
+
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('news/create', 'add')->name('news.add');
+    Route::post('news/create', 'create')->name('news.create');
+    Route::get('news', 'index')->name('news.index');
+    Route::get('news/edit', 'edit')->name('news.edit');
+    Route::post('news/edit', 'update')->name('news.update');
+    Route::get('news/delete', 'delete')->name('news.delete');
+});
+
+
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('profile/create', 'add')->name('profile.add');
+    Route::post('profile/create', 'create')->name('profile.create');
+    Route::get('profile', 'index')->name('profile.index');
+    Route::get('profile/edit', 'edit')->name('profile.edit');
+    Route::post('profile/edit', 'update')->name('profile.update');
+    Route::get('profile/delete', 'delete')->name('profile.delete');
+});
+
+use App\Http\Controllers\NewsController as PublicNewsController;
+Route::get('/', [PublicNewsController::class, 'index'])->name('news.index');
+
+//PHP/Laravel 19 課題
+use App\Http\Controllers\ProfileController as PublicProfileController;
+Route::get('/profile', [PublicProfileController::class, 'index'])->name('profile.index');
+
+
+
+
+
+
+
+/*↑にあるのととほぼ同じだから消したいが、怖いので残す。今後は綺麗に書いていくようにする。
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('news/create', 'add')->name('news.add');
+    Route::post('news/create', 'create')->name('news.create');
+    Route::get('news', 'index')->name('news.index');
+    Route::get('news/edit', 'edit')->name('news.edit');
+    Route::post('news/edit', 'update')->name('news.update');
+});
+*/
+
+
+
+/*
+/*PHP/Laravel 09 Routingについて理解する課題３　「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください */
+
+/*use App\Http\Controllers\Admin\AAAControroller;
+Route::controller(AAAController::class)
+->prefix('admin')->group(function() {
+    Route::get('/XXX','bbb');
+});
+echo ("\n");
+*/
 
